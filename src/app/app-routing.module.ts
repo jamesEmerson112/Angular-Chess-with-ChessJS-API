6c91e09ee82aee54018d32797e30fc3bc028b15a
James Emerson (An Vo) <vo.an.2503@gmail.com>
2022-01-14 13:31:30 -0800

    add Page not found and experiments without routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChessboardComponent } from './chessboard/chessboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'chess', component: ChessboardComponent},
+  {path: '', redirectTo: '/chessboard-component', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { ChessboardComponent } from './chessboard/chessboard.component';
 import { ChessGameComponent } from './chess-game/chess-game.component';
 import { ChatBoxComponent } from './chat-box/chat-box.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChessboardComponent,
     ChessGameComponent,
-    ChatBoxComponent
+    ChatBoxComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
